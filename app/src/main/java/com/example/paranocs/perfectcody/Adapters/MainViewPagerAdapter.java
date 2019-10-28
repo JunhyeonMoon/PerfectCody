e1e97ac5af6e9c7874d27219d1516fd23c3b14ad
Junhyeon Moon <paranocs@gmail.com>
2019-10-28 23:14:51 +0900

    add mainFeed viewPager

---
+++ app/src/main/java/com/example/paranocs/perfectcody/Adapters/MainViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.paranocs.perfectcody.Adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.paranocs.perfectcody.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainViewPagerAdapter extends PagerAdapter {
+    private ArrayList<HashMap<String, Object>> items = new ArrayList<>();
+    private Context mContext;
+
+    public MainViewPagerAdapter(Context context, ArrayList<HashMap<String, Object>> items){
+        this.items = items;
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+        View view = null;
+        LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        view = inflater.inflate(R.layout.main_pager, container, false);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.common_google_signin_btn_icon_light, mContext.getTheme()));
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+}
--- app/src/main/java/com/example/paranocs/perfectcody/MainActivity.java
+++ app/src/main/java/com/example/paranocs/perfectcody/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.paranocs.perfectcody;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.paranocs.perfectcody.Adapters.MainViewPagerAdapter;
+import com.example.paranocs.perfectcody.Utils.VerticalViewPager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = getClass().getName();
+    private Context mContext;
+    private ImageView imageView_home;
+    private ImageView imageView_search;
+    private ImageView imageView_favorite;
+    private ImageView imageView_profile;
+    private MainViewPagerAdapter mainViewPagerAdapter;
 
+
+    private VerticalViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+
+
+        imageView_home = findViewById(R.id.imageView_home);
+        imageView_search = findViewById(R.id.imageView_search);
+        imageView_favorite = findViewById(R.id.imageView_favorite);
+        imageView_profile = findViewById(R.id.imageView_profile);
+        viewPager = (VerticalViewPager) findViewById(R.id.viewPager);
+
+        ArrayList<HashMap<String, Object>> items = new ArrayList<>();
+        HashMap<String, Object> item = new HashMap<>();
+        item.put("aa", 11);
+        items.add(item);
+        items.add(item);
+        items.add(item);
+        mainViewPagerAdapter = new MainViewPagerAdapter(mContext, items);
+        viewPager.setAdapter(mainViewPagerAdapter);
+
+        Log.d(TAG, "viewPager current position " + viewPager.getCurrentItem());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
     }
 }
